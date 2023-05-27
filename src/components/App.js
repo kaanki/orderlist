5955cbf95900bb4e74d6bd0a02416065d1f73bb6
kaan keles <kaan.keles@mail.com>
2023-05-27 19:07:18 +0300

    Axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FoodCreate from "./FoodCreate";
 import FoodList from "./FoodList";
+import axios from "axios";
 
 const App = () => {
   const [foods, setFoods] = useState([]);
-  const editFoodById = (id,newTitle) => {
+  const fetchFoods = async () => {
+    const response = await axios.get("http://localhost:3001/foods");
+    setFoods(response.data);
+  };
+
+  useEffect(() => {
+    fetchFoods();
+  }, []);
+
+  const editFoodById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/foods/${id}`, {
+      title: newTitle,
+    });
+    console.log(response);
     const updatedFoods = foods.map((food) => {
       if (food.id === id) {
-        return {...food, title: newTitle}
+        return { ...food, ...response.data };
       }
-      return food
-    })
-    setFoods(updatedFoods)
-  }
-  const deleteFoodById = (id) => {
-    console.log("id geldi " + id);
-    const updatedFoods = foods.filter((food) => {
-        return food.id !== id;
+      return food;
     });
     setFoods(updatedFoods);
   };
-  const createFood = (title) => {
-    //foods.push({ id: 123, title }) length değişkeni güncellenmiyor array referansından dolayı
-    const updatedFoods = [
-      ...foods,
-      { id: Math.round(Math.random() * 999), title },
-    ];
+
+  const deleteFoodById = async (id) => {
+    await axios.delete(`http://localhost:3001/foods/${id}`)
+    const updatedFoods = foods.filter((food) => {
+      return food.id !== id;
+    });
+    setFoods(updatedFoods);
+  };
+
+  const createFood = async (title) => {
+    // //foods.push({ id: 123, title }) length değişkeni güncellenmiyor array referansından dolayı
+    // const updatedFoods = [
+    //   ...foods,
+    //   { id: Math.round(Math.random() * 999), title },
+    // ];
+    // setFoods(updatedFoods);
+    const response = await axios.post("http://localhost:3001/foods", {
+      title,
+    });
+    const updatedFoods = [...foods, response.data];
     setFoods(updatedFoods);
   };
 
